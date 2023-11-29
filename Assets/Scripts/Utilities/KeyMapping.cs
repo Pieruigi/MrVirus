43e553e375eef5dbb4fa8c466964b684315c8c66
Pieruigi <pierluigi.zomparelli@gmail.com>
2023-11-29 19:23:56 +0100

    player controller wip

---
+++ Assets/Scripts/Utilities/KeyMapping.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Virus
+{
+    public class KeyMapping
+    {
+        public const KeyCode MoveForward = KeyCode.W;
+        public const KeyCode MoveBackward = KeyCode.S;
+        public const KeyCode StrafeLeft = KeyCode.A;
+        public const KeyCode StrafeRight = KeyCode.D;
+
+
+    }
+
+}
